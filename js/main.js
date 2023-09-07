a4cf065b892e55ef93e354c3255e53e3601f5754
Hoang Vu <vuhh7995@gmail.com>
2023-09-08 00:03:57 +0900

    init cell element list

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+import { TURN } from "./constants.js";
 import {
   getCellElementAtIdx,
   getCellElementList,
@@ -5,14 +6,41 @@ import {
   getGameStatusElement,
 } from "./selectors.js";
 
-console.log(getCellElementList());
-console.log(getCellElementAtIdx(4));
-console.log(getCurrentTurnElement());
-console.log(getGameStatusElement());
+function toggleTurn() {
+  currentTurn = currentTurn === TURN.CIRCLE ? TURN.CROSS : TURN.CIRCLE;
+  // update turn on DOM element
+  const currentTurnElement = getCurrentTurnElement();
+  console.log("test", currentTurnElement);
+  if (currentTurnElement) {
+    currentTurnElement.classList.remove(TURN.CIRCLE, TURN.CROSS);
+    currentTurnElement.classList.add(currentTurn);
+  }
+}
+function handleCellClick(cell, index) {
+  const isClicked =
+    cell.classList.contains(TURN.CIRCLE) || cell.classList.contains(TURN.CROSS);
+  if (isClicked) return;
+  // set selected cell
+  cell.classList.add(currentTurn);
+  toggleTurn();
+  // toggle turn
+  console.log("click", cell, index);
+}
+function initCellElementList() {
+  const cellElementList = getCellElementList();
+  cellElementList.forEach((cell, index) => {
+    cell.addEventListener("click", () => handleCellClick(cell, index));
+  });
+}
+
+// console.log(getCellElementList());
+// console.log(getCellElementAtIdx(4));
+// console.log(getCurrentTurnElement());
+// console.log(getGameStatusElement());
 /**
  * Global variables
  */
-let currentTurn = "cross";
+let currentTurn = TURN.CROSS;
 let isGameEnded = false;
 let cellValues = new Array(9).fill("");
 
@@ -31,3 +59,8 @@ let cellValues = new Array(9).fill("");
  * 4. On replay button click --> reset game to play again.
  *
  */
+(() => {
+  // bind click event for all li element
+  getCurrentTurnElement();
+  initCellElementList();
+})();
